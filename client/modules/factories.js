8e28df8e6700ae2cbac4d16488cf29f723ea6ffe
joshWyatt <joshuawyatt123@gmail.com>
2014-07-07 16:25:26 -0700

    Create factories module. Begin filling out `waveService` service, to create a new wave instance with appropriate helper methods.

---
+++ client/modules/factories.js
@@ -0,0 +1,28 @@
+angular.module('wwa.factories', [])
+
+.factory('waveFactory', function(){
+
+  var waveService = {};
+  //storage for waves
+  var _waves = {};
+  var _waveIdTicker = 0;
+
+  //HELPER METHODS
+
+  //wave making function
+  waveService.makeWave = function(){
+    var wave = {};
+    wave.id = _waveIdTicker++;
+    return wave;
+    //time created
+    //
+  };
+
+  return waveService;
+
+  //add wave to waves
+  //get wave by id
+  //get wave by other properties
+  //delete waves
+
+});
