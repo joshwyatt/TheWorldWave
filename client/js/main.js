e5d3012f96cf447b4206671dc689bcfd56406c43
joshWyatt <joshuawyatt123@gmail.com>
2014-07-07 12:25:41 -0700

    Create UserController and add test paragraph in `index.html` to display user.username from controller.

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,13 @@
-var app = angular.module('wordWaveApp', []);
+var app = angular.module('worldWaveApp', [])
+
+.run(function($rootScope){
+  // Add any functionality that needs to run prior to the rest of the app.
+})
+
+.controller('UserController', function($scope){
+
+  $scope.user = {
+    username: 'josh'
+  }
+
+});
