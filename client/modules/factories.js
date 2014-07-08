06c341121a8da33d9be9a36187827b9aba60fe33
joshWyatt <joshuawyatt123@gmail.com>
2014-07-08 11:36:28 -0700

    Code cleanup. Begin adding makeWave function.

--- client/modules/factories.js
+++ client/modules/factories.js
@@ -1,14 +1,14 @@
 angular.module('wwa.factories', ['firebase', 'worldWaveApp'])
 
-.factory('waveFactory', function($firebase, fbUrl){
+.factory('waveFactory', function($firebase, fbUrl, $log){
   var waveService = {};
 
-  var wavesRef = new Firebase(fbUrl + '/waves');
-  var waves = $firebase(wavesRef);
-
-  waveService.makeWave = function(createdBy){
+  waveService.makeWave = function(){
+    var wavesRef = new Firebase(fbUrl + '/waves');
+    var waves = $firebase(wavesRef);
+    console.log('in make wave');
     var createdAt = new Date;
-    var createdBy = createdBy || 'ya moms shiver';
+    var createdBy = 'ya moms shiver';
     waves.$add({
       createdAt: createdAt,
       createdBy: createdBy
@@ -30,21 +30,16 @@ angular.module('wwa.factories', ['firebase', 'worldWaveApp'])
     var userRef = new Firebase(fbUrl + '/users');
     var auth = new FirebaseSimpleLogin(userRef, function(error, user) {
       if (error) {
-        // an error occurred while attempting login
         console.log(error);
       } else if (user) {
         console.log('User ID: ' + user.uid + ', Provider: ' + user.provider);
       }
     });
+
     auth.createUser(newUser.email, newUser.password, function(error, user) {
-      // console.log('email' + email);
-      // console.log('password' + password);
       if (!error) {
         console. log('User Id: ' + user.uid + ', Email: ' + user.email);
         auth.$add({id: user.uid, email: user.email});
-        // user.email = email;
-        // user.password = password;
-        // console.log('inside createUser');
       }
       if (error){
         console.log('in error: ' + error);
