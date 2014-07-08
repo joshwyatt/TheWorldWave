c647685d593d1215de4b08f497da55d8ad7139b1
joshWyatt <joshuawyatt123@gmail.com>
2014-07-07 19:44:36 -0700

    Refactor controllers and factories to use firebase.

--- client/modules/controllers.js
+++ client/modules/controllers.js
@@ -1,19 +1,12 @@
 angular.module('wwa.controllers', ['wwa.factories', 'firebase', 'worldWaveApp'])
 
-.controller('UserController', function($scope, userFactory, $firebase, fireFactory, fbUrl){
-  $scope.name = 'josh';
-  $scope.msg = 'I want to figure this out and get coffee';
-  var ref = new Firebase(FBURL + '/users');
-  $scope.fireUsers = $firebase(ref);
-  var addadd = function(){
-    $scope.messages.$add({from: $scope.name, body: $scope.msg});
-  };
-  $scope.fireUsers.$add({from: $scope.name, body: $scope.msg});
+.controller('UserController', function($scope, userFactory, $firebase){
+  $scope.makeUser = userFactory.makeUser;
 })
 
 .controller('WaveStatsController', function($scope, $firebase){
 })
 
 .controller('WaveController', function($scope, waveFactory, $firebase){
-  $scope.wave = waveFactory.makeWave();
+  $scope.makeWave = waveFactory.makeWave;
 });
